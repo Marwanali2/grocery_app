d6b7d6239d529a16406fce3187d40a3ee056bfa3
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2023-12-04 20:23:45 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:grocery_app/consts/theme_data.dart';
+import 'package:grocery_app/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool _isDarkTheme = false;
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: Styles.themeData(_isDarkTheme, context),
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen());
+  }
+}
